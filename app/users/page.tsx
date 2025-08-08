7d35a32fb6ef35269893edb47ed2dc13bdd35a44
Martin Ojom <martinojom28@gmail.com>
2025-08-08 19:29:36 +0200

    Programatic Navigation

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,40 +1,21 @@
-// app/users/page.tsx
-
-export const dynamic = "force-dynamic"; // ✅ Required for dynamic query param access
-
 import React from "react";
 import UserTable from "./UserTable";
-// import { SearchParamsContext } from "next/dist/shared/lib/hooks-client-context.shared-runtime";
-
-// interface Props {
-//   searchParams?: {
-//     sortOrder?: string;
-//   };
-// }
-
-// const UsersPage = async ({ searchParams }: Props) => {
-//   const sortOrder = searchParams?.sortOrder ?? "name";
-
-//   return (
-//     <>
-//       <h1>Users</h1>
-//       <UserTable sortOrder={sortOrder} />
-//     </>
-//   );
-// };
-
-// export default UsersPage;
+import Link from "next/link";
 
 const UsersPage = async ({
   params,
 }: {
-  params: Promise<{ searchParams: string }>;
+  params: Promise<{ searchParams: { sortOrder: string } }>;
 }) => {
-  const sortOrder = (await params).searchParams;
+  const { searchParams } = await params;
+  const sortOrder = searchParams?.sortOrder;
 
   return (
     <>
       <h1>Users</h1>
+      <Link href="/users/new" className="btn ">
+        New User
+      </Link>
       <UserTable sortOrder={sortOrder} />
     </>
   );
