9730571f229277df59b09ec79e1e7d38e5f56180
Martin Ojom <martinojom28@gmail.com>
2025-08-09 19:32:09 +0200

    Creating an Object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,10 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function GET(
+export async function GET(
   request: NextRequest,
   { params }: { params: { id: number } }
 ) {
-  if (params.id > 10)
+  const { id } = await params;
+  if (id > 10)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   return NextResponse.json({ id: 1, name: "Martin" });
 }
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -3,7 +3,18 @@ import { NextRequest, NextResponse } from "next/server";
 // Get - getting data
 export function GET(request: NextRequest) {
   return NextResponse.json([
-    { id: 1, name: "John Doe" },
+    { id: 1, name: "Martin" },
     { id: 2, name: "Jane Smith" },
   ]);
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  // Here you would typically save the data to a database
+  //   Validate
+  // If invalid, return 400
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  //   Else, return Data
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
 }
