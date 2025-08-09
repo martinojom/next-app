566f376a4506b4c62ca512a1f404e4b58faa1528
Martin Ojom <martinojom28@gmail.com>
2025-08-09 20:05:56 +0200

    Updating objects

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -2,10 +2,25 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { id: number } }
+  { params }: { params: Promise<{ id: number }> }
 ) {
   const { id } = await params;
   if (id > 10)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   return NextResponse.json({ id: 1, name: "Martin" });
+}
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: number }> }
+) {
+  const { id } = await params;
+  const body = await request.json();
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+
+  if (id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  return NextResponse.json({ id: 1, name: body.name }, { status: 200 });
 }
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -10,11 +10,9 @@ export function GET(request: NextRequest) {
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  // Here you would typically save the data to a database
-  //   Validate
-  // If invalid, return 400
+
   if (!body.name)
     return NextResponse.json({ error: "Name is required" }, { status: 400 });
-  //   Else, return Data
+
   return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
 }
