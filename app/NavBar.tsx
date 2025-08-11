de9599537fe6fce30c7409252e1fb6eac42e81dc
Martin Ojom <martinojom28@gmail.com>
2025-08-12 00:47:18 +0200

    User session on the client side

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,12 @@
+"use client";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
+import Loading from "./loading";
 
 const NavBar = () => {
+  const { status, data: session } = useSession();
+
   return (
     <div className="flex bg-slate-200 p-5 space-x-3">
       <Link href="/" className="mr-5">
@@ -10,9 +15,13 @@ const NavBar = () => {
       <Link href="/users" className="mr-5">
         Users
       </Link>
-      <Link href="/api/auth/signin" className="mr-5">
-        Login
-      </Link>
+      {status === "loading" && <Loading />}
+      {status === "authenticated" && <div>{session.user!.name}</div>}
+      {status === "unauthenticated" && (
+        <Link href="/api/auth/signin" className="mr-5">
+          Login
+        </Link>
+      )}
     </div>
   );
 };
