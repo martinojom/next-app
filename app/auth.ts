4aada64e610138842c498cfe274136b5916152c0
Martin Ojom <martinojom28@gmail.com>
2025-08-12 00:19:17 +0200

    Understanding Authentication Session

--- app/auth.ts
+++ app/auth.ts
@@ -2,10 +2,6 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
-// export const { handlers, auth } = NextAuth({
-//   providers: [Resend],
-// });
-
 export const { auth, handlers, signIn, signOut } = NextAuth({
   providers: [GitHub, Google],
 });
