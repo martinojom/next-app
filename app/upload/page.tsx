5f6a6eb25c04ca19d1c3bf553ef640f856b0f811
Martin Ojom <martinojom28@gmail.com>
2025-08-11 13:22:47 +0200

    Uploading Images to Cloudinary Cloud with CldUploadWidget uploadPreset=""

---
+++ app/upload/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+import { CldUploadWidget } from "next-cloudinary";
+
+const UploadPage = () => {
+  return (
+    <CldUploadWidget uploadPreset="om4673mo">
+      {({ open }) => (
+        <button className="btn btn-primary" onClick={() => open()}>
+          Upload
+        </button>
+      )}
+    </CldUploadWidget>
+  );
+};
+
+export default UploadPage;
