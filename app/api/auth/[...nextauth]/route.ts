2b2078da5ceb3387bdb66adda2c836630f62c780
Martin Ojom <martinojom28@gmail.com>
2025-08-12 22:32:43 +0200

    Creating a database Adapter

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,14 @@
 import NextAuth, { NextAuthConfig } from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "@/prisma/client";
 
 export const authOptions: NextAuthConfig = {
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
   providers: [
     GitHub({
       clientId: process.env.AUTH_GITHUB_ID,
