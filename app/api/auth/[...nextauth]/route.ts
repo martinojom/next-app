b62411c67de0d6bee7d2212835518785e76d134f
Martin Ojom <martinojom28@gmail.com>
2025-08-13 00:21:40 +0200

    Configuring CredintialProvider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,10 @@
 import NextAuth, { NextAuthConfig } from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "@/prisma/client";
+import bcrypt from "bcrypt";
 
 export const authOptions: NextAuthConfig = {
   adapter: PrismaAdapter(prisma),
@@ -10,6 +12,35 @@ export const authOptions: NextAuthConfig = {
     strategy: "jwt",
   },
   providers: [
+    Credentials({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "Email" },
+        password: { label: "Password", type: "text", placeholder: "Password" },
+      },
+
+      async authorize(credentials) {
+        const email = credentials?.email as string;
+        const password = credentials?.password as string;
+
+        if (!email || !password) return null;
+
+        const user = await prisma.user.findUnique({
+          where: { email: email },
+        });
+
+        if (!user) return null;
+
+        const isPasswordValid = await bcrypt.compare(
+          password,
+          user.hashedPassword!
+        );
+
+        if (!isPasswordValid) return null;
+
+        return user;
+      },
+    }),
     GitHub({
       clientId: process.env.AUTH_GITHUB_ID,
       clientSecret: process.env.AUTH_GITHUB_SECRET,
