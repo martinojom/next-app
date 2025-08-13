6eaca85ebefc4dc592d5fcebbd63a6d90e5faa8b
Martin Ojom <martinojom28@gmail.com>
2025-08-13 16:44:57 +0200

    Sending Emails with resend

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -3,6 +3,7 @@ import {
   Html,
   Body,
   Container,
+  Tailwind,
   Text,
   Link,
   Preview,
@@ -12,12 +13,14 @@ const WelcomeTemplate = ({ name }: { name: string }) => {
   return (
     <Html>
       <Preview>Welcome aboard</Preview>
-      <Body>
-        <Container>
-          <Text>Hello {name}</Text>
-          <Link href="https://martinojom.com">www.martinojom.com</Link>
-        </Container>
-      </Body>
+      <Tailwind>
+        <Body className="bg-white">
+          <Container>
+            <Text className="font-bold text-3xl">Hello {name}</Text>
+            <Link href="https://martinojom.com">www.martinojom.com</Link>
+          </Container>
+        </Body>
+      </Tailwind>
     </Html>
   );
 };
